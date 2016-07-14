3febea096e1f3953fb442e9b021d1d1cdb32bf9f
rafal <you@example.com>
2016-07-14 18:12:15 +0200

    init flag

--- C/GeneralAlgorithms/Codility/flag.c
+++ C/GeneralAlgorithms/Codility/flag.c
@@ -5,26 +5,31 @@ Look at https://codility.com/demo/results/trainingDYRQDX-WCV/
 /* Solution 100% - Time complexity O(N) */
 
 int solution(int A[], int N) {
-  // write your code in C99 (gcc 4.8.2)
-  int i = 0;
-  long int rsum = 0, lsum = 0;
-      
-  if (N < 1)
-    return -1;
-  
-  for (i = 0; i < N; i++) 
-    rsum += A[i]; 
-  
-  for (i = 0; i < N; i++)  {
-    rsum -= A[i];    
-      
-    if (lsum == rsum) 
-      return i;      
-      
-    lsum += A[i];
-  }   
-  
-  return -1;
+    // write your code in C99 (gcc 4.8.2)
+    int peaks[N];
+    int k = 0;
+    int nr = 0;
+    int max = 0;
+    int left, right;
+    
+    nr = fillPeak(peaks, A, N);
+        
+    left = 1;
+    right = nr;
+     
+    //Binary search             
+    while (left <= right) {
+        k = (right + left) / 2;          
+     
+        if (check(k, peaks, nr) == 1) {          
+            max = k;            
+            left = k + 1;
+        }
+        else
+            right = k - 1;        
+    }
+    
+    return max;
 }
 
 int fillPeak(int *p, int *A, int N) {
@@ -69,8 +74,7 @@ int solution(int A[], int N) {
         
     left = 1;
     right = nr;
-     
-    //Binary search             
+                  
     while (left <= right) {
         k = (right + left) / 2;          
      
@@ -84,3 +88,4 @@ int solution(int A[], int N) {
     
     return max;
 }
+
