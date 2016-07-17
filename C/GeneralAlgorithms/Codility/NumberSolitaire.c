315b2d29f7e41de04cb245c2d47be9fcf3e526fe
rafal <you@example.com>
2016-07-17 19:48:51 +0200

    delete not needed code

--- C/GeneralAlgorithms/Codility/NumberSolitaire.c
+++ C/GeneralAlgorithms/Codility/NumberSolitaire.c
@@ -3,33 +3,8 @@ Look at https://codility.com/demo/results/demoQD6SQT-KRJ/
 */
 
 /* Solution 100% - Time complexity O(N) */
-/*Dynamic programming problem */
+/* Dynamic programming problem */
 
-int solution(int A[], int N) {
-  // write your code in C99 (gcc 4.8.2)
-  int i = 0;
-  long int rsum = 0, lsum = 0;
-      
-  if (N < 1)
-    return -1;
-  
-  for (i = 0; i < N; i++) 
-    rsum += A[i]; 
-  
-  for (i = 0; i < N; i++)  {
-    rsum -= A[i];    
-      
-    if (lsum == rsum) 
-      return i;      
-      
-    lsum += A[i];
-  }   
-  
-  return -1;
-}
-
-// you can write to stdout for debugging purposes, e.g.
-// printf("this is a debug message\n");
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
 int solution(int A[], int N) {
